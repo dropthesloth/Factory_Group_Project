864b7c174191b71ab9c1aaf43e886eb665355389
dropthesloth <nathaniel99@live.nl>
2025-01-16 15:31:11 +0100

    Added Note script and TextMeshPro
    Added script for showing notes and TextMeshPro to the assets

---
+++ Assets/Scenes/TestScenes/Nathans Assets/Scripts/NoteScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteScript : MonoBehaviour
+{
+    public GameObject note;
+    // Start is called before the first frame update
+    void Start()
+    {
+        note.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (note.activeSelf && Input.GetKeyDown(KeyCode.Escape) /*|| Input.GetMouseButtonDown(0)*/)
+            note.SetActive(false);
+    }
+    void OnMouseDown()
+    {
+        Debug.Log("clicked note");
+        //if (!note.activeSelf)
+            note.SetActive(true);
+    }
+}
