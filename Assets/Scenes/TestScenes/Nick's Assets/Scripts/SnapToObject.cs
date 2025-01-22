202ada47703ca8ed5402d15b3b9f2cb172827343
NickvanAmersfoort1 <nickvanamersfoort86@gmail.com>
2025-01-22 12:52:21 +0100

    Checking if the object is in the right place
    It works, there might be some bugs (if you find any lmk)

--- Assets/Scenes/TestScenes/Nick's Assets/Scripts/SnapToObject.cs
+++ Assets/Scenes/TestScenes/Nick's Assets/Scripts/SnapToObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,20 +8,22 @@ public class SnapToObject : MonoBehaviour
 {
     public PickUpObjects player;
     private Vector3 originalObjectLocation;
-    private bool isObjectAttached = false;
+    public bool isObjectAttached = false;
     private void OnTriggerStay(Collider other)
     {
-        
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Input.GetKeyDown(KeyCode.F) && isObjectAttached == false)
         {
             originalObjectLocation = this.transform.position;
             player.isObjectPickedUp = false;
+            this.GetComponent<Rigidbody>().isKinematic = true;
             this.gameObject.transform.position = other.transform.position;
             isObjectAttached = true;
             
-        }else if (Input.GetKeyDown(KeyCode.F) && isObjectAttached == true)
+        }else if (Input.GetKeyDown(KeyCode.F) && isObjectAttached == true && Physics.Raycast(ray.origin,ray.direction * 10))
         {
             player.isObjectPickedUp = true;
+            this.GetComponent<Rigidbody>().isKinematic = false;
             this.gameObject.transform.position = originalObjectLocation;
             isObjectAttached = false;
         }
