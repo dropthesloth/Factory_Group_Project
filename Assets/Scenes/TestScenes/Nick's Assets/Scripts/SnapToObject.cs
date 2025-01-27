29f303ee0a774990a3561354eb71bf26e27f4a8c
NickvanAmersfoort1 <nickvanamersfoort86@gmail.com>
2025-01-27 12:54:23 +0100

    Picking up objects
    Everything works with the check included!

--- Assets/Scenes/TestScenes/Nick's Assets/Scripts/SnapToObject.cs
+++ Assets/Scenes/TestScenes/Nick's Assets/Scripts/SnapToObject.cs
@@ -3,29 +3,64 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.ProBuilder;
+using UnityEngine.UI;
 
 public class SnapToObject : MonoBehaviour
 {
+    public TextMeshProUGUI attachToObject;
     public PickUpObjects player;
-    private Vector3 originalObjectLocation;
     public bool isObjectAttached = false;
+
+    private void Awake()
+    {
+        //attachToObject.gameObject.SetActive(false);
+    }
     private void OnTriggerStay(Collider other)
     {
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Input.GetKeyDown(KeyCode.F) && isObjectAttached == false)
+        if (isObjectAttached == false)
         {
-            originalObjectLocation = this.transform.position;
-            player.isObjectPickedUp = false;
-            this.GetComponent<Rigidbody>().isKinematic = true;
-            this.gameObject.transform.position = other.transform.position;
-            isObjectAttached = true;
-            
-        }else if (Input.GetKeyDown(KeyCode.F) && isObjectAttached == true && Physics.Raycast(ray.origin,ray.direction * 10))
-        {
-            player.isObjectPickedUp = true;
-            this.GetComponent<Rigidbody>().isKinematic = false;
-            this.gameObject.transform.position = originalObjectLocation;
-            isObjectAttached = false;
+            //attachToObject.gameObject.SetActive(true);
         }
+        else
+        {
+            //attachToObject.gameObject.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (isObjectAttached)
+            {
+                if (Physics.Raycast(ray.origin, ray.direction, out RaycastHit hit, 10, LayerMask.GetMask("pickUpObject")) && player.pickedupObject == null)
+                {
+                    Debug.Log("This part works");
+                    if (hit.collider.gameObject == this.gameObject)
+                    {
+                        Debug.Log("pickedUpObject was not null!");
+                        player.isObjectPickedUp = true;
+                        this.GetComponent<Rigidbody>().isKinematic = false;
+                        this.transform.position = new Vector3(Camera.main.transform.localPosition.x, Camera.main.transform.localPosition.y - 1, Camera.main.transform.localPosition.z + 2);
+                        player.pickedupObject = this.gameObject;
+                        player.pickedupObject.transform.SetParent(Camera.main.transform);
+                        player.holdingObject();
+                        isObjectAttached = false;
+                    }
+                }
+            }
+            else
+            {
+                player.isObjectPickedUp = false;
+                this.GetComponent<Rigidbody>().isKinematic = true;
+                player.pickedupObject.transform.SetParent(null);
+                player.pickedupObject = null;
+                this.transform.position = other.transform.position;
+                isObjectAttached = true;
+            }  
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //attachToObject.gameObject.SetActive(false);
     }
 }
